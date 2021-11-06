c92f73ddd4fe16f295e999e9298d9ac818e08df3
EduardoV17 <eduar.vel7@gmail.com>
2021-11-05 21:27:24 -0600

    prueba 2

--- Principal.java
+++ Principal.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 import Servidor.Controlador.CtrlPrincipal;
-// Que chingue su madre el america
+// Que chingue su madre el Cruz azul ola
 public class Principal{
     public static void main(String[] args) {
         new CtrlPrincipal();
